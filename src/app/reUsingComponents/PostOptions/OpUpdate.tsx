01772c09eadabe2fa58cd9aaff84534241b3714f
hungaa1 <nguyentronghung05072003@gmail.com>
2024-02-19 21:56:01 +0700

    ok

--- src/app/reUsingComponents/PostOptions/OpUpdate.tsx
+++ src/app/reUsingComponents/PostOptions/OpUpdate.tsx
@@ -1,4 +1,4 @@
-import { CloseI } from '~/assets/Icons/Icons';
+import { CloseI, GarbageI } from '~/assets/Icons/Icons';
 import { DivPos } from '../styleComponents/styleComponents';
 import { Div, P } from '../styleComponents/styleDefault';
 
@@ -6,20 +6,21 @@ const OpUpdate: React.FC<{ createdAt: string; setOptions: React.Dispatch<React.S
     createdAt,
     setOptions,
 }) => {
+    const options = [{ type: "Post's data", children: [{ id: 1, name: 'Move to trash', icon: <GarbageI /> }] }];
     return (
         <Div
-            width="300px"
-            wrap="wrap"
+            width="100%"
             css={`
-                height: 84%;
-                position: absolute;
+                height: 100%;
+                position: fixed;
                 right: 0;
-                top: 30px;
-                z-index: 1;
-                background-color: #292a2d;
+                align-items: end;
+                top: 0px;
+                z-index: 999;
+                background-color: #151515b0;
             `}
         >
-            <Div
+            {/* <Div
                 width="100%"
                 css={`
                     height: 35px;
@@ -27,12 +28,29 @@ const OpUpdate: React.FC<{ createdAt: string; setOptions: React.Dispatch<React.S
                     align-items: center;
                 `}
             >
-                <P z="1.5rem">Optional Update</P>
                 <DivPos top="8px" right="8px" size="20px" onClick={() => setOptions('')}>
                     <CloseI />
                 </DivPos>
+            </Div> */}
+            <Div
+                width="100%"
+                display="block"
+                css="height: auto;padding: 5px;background-color: #fff; color: #171717; font-size: 1.5rem; border-top-left-radius: 5px; border-top-right-radius: 5px;"
+            >
+                {options.map((o) => (
+                    <Div key={o.type} display="block" css={``}>
+                        <P css="border-top-left-radius: 5px;border-top-right-radius: 5px; padding-left: 5px; background-color: #b8b8b8; font-weight: 600;">
+                            {o.type}
+                        </P>
+                        {o.children.map((c) => (
+                            <Div key={c.id} css="align-items: center;">
+                                <Div>{c.icon}</Div>
+                                <P>{c.name}</P>
+                            </Div>
+                        ))}
+                    </Div>
+                ))}
             </Div>
-            <Div width="100%"></Div>
         </Div>
     );
 };
